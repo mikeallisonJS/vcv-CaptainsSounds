4083b3f88bb9692ea89c4d12ce7b44827a5f05c3
Mike Allison <dj.mikeallison@gmail.com>
2025-02-12 04:20:07 +0000

    Update devcontainer configuration and Dockerfile

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+set -e
+
 rm -rf Rack-SDK
-wget https://vcvrack.com/downloads/Rack-SDK-latest-lin-x64.zip
-unzip Rack-SDK-latest-lin-x64.zip
-rm Rack-SDK-latest-lin-x64.zip
+wget https://vcvrack.com/downloads/Rack-SDK-2.6.0-lin-x64.zip
+unzip Rack-SDK-2.6.0-lin-x64.zip
+rm Rack-SDK-2.6.0-lin-x64.zip
+
+# Ensure proper permissions
+chmod -R 755 Rack-SDK
+
+# Create dep directory if it doesn't exist
+mkdir -p dep
+
+# Initialize git submodules if any
+git submodule update --init --recursive
+
+# Run make dep to fetch dependencies
+make dep
